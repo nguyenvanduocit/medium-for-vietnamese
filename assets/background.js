20700646fbb01efa1cd67e2fa3fd9dc236eb883c
Duoc <duoc@construct.sg>
2017-12-18 16:48:32 +0700

    support goodreads

--- assets/background.js
+++ assets/background.js
@@ -1,11 +1,20 @@
+const fileMap = {
+  'https://glyph.medium.com/css/e/sr/latin/e/ssr/latin/e/ssb/latin/m2.css': 'assets/css/medium.css',
+  'https://s.gr-assets.com/assets/gr/fonts-e256f84093cc13b27f5b82343398031a.css': 'assets/css/goodreads.css'
+}
+
 const handler = (info) => {
+  console.log(info)
   if (info.tabId === -1) {
     return {}
   }
-  return {redirectUrl: chrome.extension.getURL('assets/fontface.css')}
+  console.log(fileMap[info.url])
+  return {
+    redirectUrl: chrome.extension.getURL(fileMap[info.url])
+  }
 }
 const options = {
-  urls: ['https://glyph.medium.com/css/e/sr/latin/e/ssr/latin/e/ssb/latin/m2.css'],
+  urls: Object.keys(fileMap),
   types: ['stylesheet']
 }
 /* global chrome */
